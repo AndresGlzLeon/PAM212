0d34695d61ba78c0b8d9f35759eb81e950b336e0
AndresGlzLeon <124049210@upq.edu.mx>
2025-09-09 19:58:02 -0500

    Agregué tipos de datos.

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -3,3 +3,17 @@ document.write("<h1> Hello world! </h1>");
 console.log("Hello world, again.");
 
 console.error("Mi error personalizado. ");
+
+"Esto es un string" //String
+"Aca hay otro xd" //String
+
+123  //Numbe
+2 //Number
+3 //Number
+
+true //boolean
+false //boolean
+
+["Juan", "Luis" , "Marco"] //array 
+[1 , "a" , false] 
+
