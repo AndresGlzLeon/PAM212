11fb70f739a892fe74257354e87b29e13876b309
AndresGlzLeon <124049210@upq.edu.mx>
2025-09-18 18:59:04 -0600

    Ejercicio 2
    Segundo ejercicio terminado.

--- Practica1/practica1.js
+++ Practica1/practica1.js
@@ -6,3 +6,16 @@ nombre = "Ana Maria";
 const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
 
 console.log(saludo);
+
+/*
+    function cuadrado(numero) {
+        return numero * numero;
+    }
+*/
+
+const cuadrado = numero => numero * numero;
+
+console.log(cuadrado(1));
+console.log(cuadrado(2));
+console.log(cuadrado(3));
+
