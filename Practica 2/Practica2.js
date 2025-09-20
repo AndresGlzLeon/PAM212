fa2ea3150061476e229dee162b53bb12863ee9fb
AndresGlzLeon <124049210@upq.edu.mx>
2025-09-19 18:54:57 -0600

    Ejercicio B
    Completé el ejercicio "B".

--- Practica 2/Practica2.js
+++ Practica 2/Practica2.js
@@ -1,3 +1,4 @@
+// EJERCICIO A
 const person = {
     nombre: "Andrés",
     edad: 19,
@@ -12,3 +13,16 @@ const { nombre, edad, direccion: { ciudad, pais }} = person;
 console.log("Me llamo ", nombre, " tengo ", edad, " años y vivo en ", ciudad , pais);
 document.write("Me llamo ", nombre, " tengo ", edad, " años y vivo en ", ciudad , pais);
 
+
+// EJERCICIO B
+const productos = [
+    {nombre: "Laptop", precio: 12000},
+    {nombre : "Mouse", precio: 250},
+    {nombre : "Teclado", precio: 750},
+    {nombre : "Monitor", precio: 3000}
+];
+
+const nombres = productos.filter(producto => producto.precio > 1000);
+productos.map(producto => console.log(producto.nombre));
+
+console.log(nombres);
