1b32317cf124701b62d5e78b30481818c40dfed0
AndresGlzLeon <124049210@upq.edu.mx>
2025-09-25 18:32:14 -0600

    Segundo ejercicio
    Segundo ejercicio terminado

--- Practica3/main.js
+++ Practica3/main.js
@@ -1,3 +1,25 @@
+//EJRCICIO A
 import {restar} from "./utils.js";
 
-console.log(restar(14, 1));
+console.log(restar(10, 1));
+
+//EJERCICIO B
+function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (usuario === "Admin") {
+                resolve("Usuario verificado");
+            } else {
+                reject("Usuario no verificado");
+            }
+        }, 2000);
+    });
+}
+
+    verificarUsuario("Admin")
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+    verificarUsuario("Andres")
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
