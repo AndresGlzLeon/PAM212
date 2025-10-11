2c13ee86c27096d8b7d89103d1d70dfe9bb157db
AndresGlzLeon <andres712leon@gmail.com>
2025-10-10 18:55:15 -0600

    Practica 6 terminada

--- intro/App.js
+++ intro/App.js
@@ -1,35 +1,14 @@
 //1.Import: Zona de declaraciones. Componentes extras,librerias,navegación etc.
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React,{useState} from 'react';
-
+import ContadorScreen from "./screens/ContadorScreen";
 
 //2. Main: Zona de componentes 
 export default function App() {
 
-  const[contador, setContador]=useState(0);
 
   return (
 
-    <View style={styles.container}>
-      
-      <Text>Contador: {contador} </Text>
-      <Button title= "Agregar" onPress={()=>setContador(contador + 1)}/>
-      <Button title= "Quitar" onPress={()=>setContador(contador - 1)}/>
-      <Button title= "Reiniciar" onPress={()=>setContador(contador + (contador * (-1)))}/>
-      <StatusBar style="auto" />
+    <ContadorScreen</ContadorScreen>
 
-    </View>
-  
   );
-}
 
-//3. Estilos: Zona de estetica y posicionamiento.
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
