feb0f0ed9ab606a88293e760e788ad403b595387
andres712leon@gmail.com <124049210@upq.edu.mx>
2025-11-07 18:52:21 -0600

    Commit Final
    Me faltó subir mi app

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Principal from './screens/Principal';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Principal></Principal>
   );
 }
 
